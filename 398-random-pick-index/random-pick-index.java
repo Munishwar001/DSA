bc7f47816896526491cdc4ade00cc01576ab2d66
Munishwar kalra <kalramunishwar@gmail.com>
2026-08-31 16:26:45 +0530

    Time: 1502 ms (35.75%) | Memory: 60.7 MB (93.46%) - LeetSync

---
+++ 398-random-pick-index/random-pick-index.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    private int[] arr;
+    private Random random;
+
+    public Solution(int[] nums) {
+        this.arr = nums;
+        this.random = new Random();
+    }
+
+    public int pick(int target) {
+        int answer = -1;
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == target) {
+
+                count++;
+
+                if (random.nextInt(count) == 0) {
+                    answer = i;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
